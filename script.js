1d554766e4077808ae2d4c17e97d997e42c57daf
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-02-11 17:38:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 //your JS code here. If required.
-describe('Modal test', () => {
-  it('should close the modal', () => {
-    // Open the modal
-    cy.get('#exampleModal').should('be.visible');
-
-    // Click the close button
-    cy.get('.close').click();
-
-    // Wait for half a second
-    cy.wait(500);
-
-    // Check that modal is not visible
-    cy.get('#exampleModal').should('not.be.visible');
-  });
-});
